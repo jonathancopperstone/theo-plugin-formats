cf556faeeadf09a15fa3bf554b7717144f2a89e0
Johnny Copperstone <jonnce@gmail.com>
2016-04-05 15:23:22 +0100

    Fix ES6 module bundling

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,9 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
     ]
+  },
+  output: {
+    library: 'theo-plugin-formats',
+    libraryTarget: 'umd'
   }
 }
